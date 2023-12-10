5e051873a5e6ee3c9f22fda51ee8ffe8236ed711
octustore <152689214+octustore@users.noreply.github.com>
2023-12-10 21:25:52 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const puppeteer = require('puppeteer');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+
+
+app.get('/', async (req, res) => {
+    try {
+        const browser = await puppeteer.launch();
+        const browserWSEndpoint = browser.wsEndpoint();
+        await browser.close();
+        res.send(browserWSEndpoint);
+    } catch (error) {
+        console.error('Error generating PDF:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
